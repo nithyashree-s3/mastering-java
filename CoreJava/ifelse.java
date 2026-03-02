2a08b4e08853636bccff5138be8e6eaadcc46b77
nithyashree-s3 <nithyasri332005@gmail.com>
2026-03-02 11:22:11 +0530

    Add ifelse.java for rain scenario demonstration

---
+++ CoreJava/ifelse.java
@@ -0,0 +1,13 @@
+class ifelse {
+    public static void main(String[] args) {
+        //Scenario 1:its going 
+        boolean rain = false;
+        if(rain)
+        {
+            System.out.print("Take an Umbrella");
+        }
+        else{
+            System.out.print("Enjoy the Sunlight");
+        }
+    }
+}
