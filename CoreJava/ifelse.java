5dd079af7d0291b73f0bf64ebeaf68c807eb93eb
nithyashree-s3 <nithyasri332005@gmail.com>
2026-04-08 12:17:37 +0530

    Update ifelse.java

--- CoreJava/ifelse.java
+++ CoreJava/ifelse.java
@@ -11,3 +11,41 @@ class ifelse {
         }
     }
 }
+
+
+//COMPARISON OPERATOR
+//1)String Comparison
+//Operator which is used to compare two values.  5>4  Here > is a xomparison operator here we are comparing two value.
+//Example Question 1
+//We have two numbers num1=40 num2=50 Check whether two number is equal or not
+class ifelse { //to check copy this code seperately
+    public static void main(String[] args) 
+    {
+        int num1 = 40;
+        int num2 = 50;
+        if(num1==num2)
+        {
+            System.out.print("Both the numbers are Equal");
+        else{
+            System.out.print("Not equal");
+        }
+    }
+}    
+
+//to get userinput for the same question
+import java.util.Scanner;
+class ifelse{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        if(num1==num2)
+        {
+            System.out.print("Both the numbers are Equal");
+        else{
+            System.out.print("Not equal");
+        }
+    }
+}    
+    
